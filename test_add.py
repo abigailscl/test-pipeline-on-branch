c7da80f15d80b9643e13fd50d2dda1c8935fef71
antonioxzz <antonioitalo.xzz@gmail.com>
2023-06-15 14:16:17 -0500

    add unit test for addition of two numbers

---
+++ test_add.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_addition_returns_result_when_have_two_numbers():
+    first_number = 3
+    second_number = 7
+    expected_result = 10
+
+    result = addition(first_number, second_number)
+
+    assert expected_result == result
