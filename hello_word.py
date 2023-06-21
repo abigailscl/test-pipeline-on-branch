417a169e99106c628950aedde14594d330dd3f9b
jhon-meza <jhon.meza@ioet.com>
2023-06-21 17:31:55 -0500

    refactoring fuction name and test

--- hello_word.py
+++
@@ -1,2 +0,0 @@
-def addition_Jhon(first_number: int, second_number: int) -> int:
-    return first_number + second_number
--- test_add.py
+++ test_add.py
@@ -1,11 +1,11 @@
 import pytest
-from hello_word import addition_Jhon
+from math_operations import addition
 
 def test_addition_returns_result_when_have_two_numbers():
     first_number = 3
     second_number = 7
     expected_result = 10
 
-    result = addition_Jhon(first_number, second_number)
+    result = addition(first_number, second_number)
 
     assert expected_result == result
