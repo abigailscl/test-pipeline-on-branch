d42d190ea2cae3f1485294142ddf7e9043d76b9c
Andres Armendariz <andy_armendariz@hotmail.es>
2023-06-21 17:32:22 -0500

    refactoring hello_word.py to math_operations.py

--- hello_word.py
+++
@@ -1,2 +0,0 @@
-def addition_andres(number_1: int, number_2) -> int:
-    return number_1 + number_2
--- test_add.py
+++ test_add.py
@@ -1,11 +1,11 @@
 import pytest
-import hello_word
+import math_operations
 
 def test_addition_returns_result_when_have_two_numbers():
     first_number = 3
     second_number = 7
     expected_result = 10
 
-    result = hello_word.addition_andres(first_number, second_number)
+    result = math_operations.addition(first_number, second_number)
 
     assert expected_result == result
