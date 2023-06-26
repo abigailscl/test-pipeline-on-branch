84688da5a18c19799a75edc10cfc4334947756ef
Andreina-Pallo <andreina.pallo@ioet.com>
2023-06-26 15:07:29 -0500

    adding addition function

---
+++ hello_word.py
@@ -0,0 +1,2 @@
+def addition_andreina(first_number: int, second_number: int):
+    return 0
--- test_add.py
+++ test_add.py
@@ -1,11 +1,10 @@
 import pytest
 
-
 def test_addition_returns_result_when_have_two_numbers():
     first_number = 3
     second_number = 7
     expected_result = 10
 
-    result = addition(first_number, second_number)
+    result = addition_andreina(first_number, second_number)
 
     assert expected_result == result
