7a0de47be62868b8a64913cbace52a6b512e2fd9
Andreina-Pallo <andreina.pallo@ioet.com>
2023-07-24 15:53:23 -0500

    refactoring addition function

--- hello_word.py
+++ hello_word.py
@@ -1,2 +1,3 @@
-def addition_andreina(first_number: int, second_number: int):
-    return 0
+def addition(number_1: int, number_2) -> int:
+    result = number_1 + number_2
+    return result
--- test_add.py
+++ test_add.py
@@ -1,10 +1,13 @@
+from hello_word import addition
+
 import pytest
 
+
 def test_addition_returns_result_when_have_two_numbers():
-    first_number = 3
-    second_number = 7
-    expected_result = 10
+     first_number = 3
+     second_number = 7
+     expected_result = 10
 
-    result = addition_andreina(first_number, second_number)
+     result = addition(first_number, second_number)
 
-    assert expected_result == result
+     assert expected_result == result
